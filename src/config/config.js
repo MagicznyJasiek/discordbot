64c4c2967e8df75e6bb5eb84c896251adb23e6d6
Magiczny Jasiek <61098959+MagicznyJasiek@users.noreply.github.com>
2024-03-22 01:03:45 +0100

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    token: "MTIxMDYwODI1NTA3NjY3MTQ4OA.GZ2Xrw.-Kvqlt76JAimtvHNSO20FwsKwjeEstTgLRapP0", // your discord token here
+    token: "MTIxMDYwODI1NTA3NjY3MTQ4OA.GZ2Xrw.-Kvqlt76JAimtvHNSO20FwsKwjeEstTgLRapP0", // your discord bot token here
     // you can put many other thing here, to your liking!
 }
